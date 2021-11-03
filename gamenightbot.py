5178e382fc0d92ff273d3a049f41b22621ee7980
conortwo <conor.t.email@gmail.com>
2021-11-03 13:08:51 -0400

    Fix tz

--- gamenightbot.py
+++ gamenightbot.py
@@ -434,7 +434,7 @@ async def cyberpunk_go_no_go(channel_id, message):
                 reminder = {"start_time": start_time, "game_name": "Cyberpunk Red"}
                 game_night = state[channel_id].get("game_night", "game day")
                 remind_at = parse(f"{start_time} {game_night}")
-                await save_state(channel_id, "remind_at", (remind_at - timedelta(hours=2)).timestamp())
+                await save_state(channel_id, "remind_at", (remind_at - timedelta(hours=1)).timestamp())
                 await save_state(channel_id, "reminder", reminder)
                 announce = f"""@everyone The poll has concluded. 
 The group has decided we'll play **Cyberpunk Red** @ **{start_time}** on **{game_night}**.
@@ -750,7 +750,7 @@ async def bonus(ctx, start_time, *gamename):
         await ctx.send(f"Sorry I had trouble understanding {start_time} as a a start time. Please try again.")
         return
     await ctx.send(f"Ok, I'll announce your suggestion of {game_name} @ {start_time} on {game_night}.")
-    await save_state(channel_id, "bonus_remind_at", (remind_at - timedelta(hours=2)).timestamp())
+    await save_state(channel_id, "bonus_remind_at", (remind_at - timedelta(hours=1)).timestamp())
     await save_state(channel_id, "bonus_reminder", reminder)
     channel = client.get_channel(int(channel_id))
     announce = f"""Okay, I've setup a ✨ **bonus** ✨ game day for {mentions}.
@@ -783,7 +783,7 @@ async def suggest(ctx, start_time, *gamename):
         await ctx.send(f"Sorry I had trouble understanding {start_time} as a a start time. Please try again.")
         return
     await ctx.send(f"Ok, I'll announce your suggestion of {game_name} @ {start_time} on {game_night}.")
-    await save_state(channel_id, "remind_at", (remind_at - timedelta(hours=2)).timestamp())
+    await save_state(channel_id, "remind_at", (remind_at - timedelta(hours=1)).timestamp())
     await save_state(channel_id, "reminder", reminder)
     channel = client.get_channel(int(channel_id))
     attendees = state[channel_id].get("attendees", [])
